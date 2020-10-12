6f946e0ed5009813a7f746e1811e00b4875dab3a
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2020-10-12 23:47:17 +0530

    webBrowserOpen has much more redability

--- lib/webBrowserOpen.dart
+++ lib/webBrowserOpen.dart
@@ -1,3 +1,5 @@
+// This file has the code related to the social media links which will be opening in app in the browser itself.
+
 import 'package:flutter_web_browser/flutter_web_browser.dart';
 import 'package:flutter/material.dart';
 
