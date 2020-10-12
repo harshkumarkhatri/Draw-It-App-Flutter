140bb7be212b3fc00bbb9b25a446b1d7cdd2ba95
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2020-10-12 23:41:27 +0530

    Removed dead code from LoginSignUpSwitcher.dart

--- lib/LogInSignUpSwitcher.dart
+++ lib/LogInSignUpSwitcher.dart
@@ -1,3 +1,5 @@
+// This page has the code related to the login signup switcher screen
+
 import 'package:draw_it_app/LoginPage.dart';
 import 'package:draw_it_app/SignUpPage.dart';
 import 'package:flutter/material.dart';
@@ -27,51 +29,51 @@ class _LogInSignUpSwitcherState extends State<LogInSignUpSwitcher>
     return Scaffold(
         appBar: PreferredSize(
           preferredSize: Size.fromHeight(125),
-          child:  AppBar(
-              backgroundColor: Colors.white,
-              title: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(height:80,width:80,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Image(
-                          height: 82,fit: BoxFit.cover,
-                          image: AssetImage("assets/appLogo.png"),
-                        ),
+          child: AppBar(
+            backgroundColor: Colors.white,
+            title: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 80,
+                    width: 80,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image(
+                        height: 82,
+                        fit: BoxFit.cover,
+                        image: AssetImage("assets/appLogo.png"),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              bottom: TabBar(
-                  controller: controller,
-                  indicatorWeight: 4,
-                  indicatorColor: Colors.blue,
-                  tabs: [
-                    Tab(
-                      // icon: Icon(Icons.lock),
-                      child: Text(
-                        "Login",
+            ),
+            bottom: TabBar(
+                controller: controller,
+                indicatorWeight: 4,
+                indicatorColor: Colors.blue,
+                tabs: [
+                  Tab(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                  ),
+                  Tab(
+                    child: Text("Sign Up",
                         style: TextStyle(
                             color: Colors.black,
                             fontWeight: FontWeight.bold,
-                            fontSize: 16),
-                      ),
-                    ),
-                    Tab(
-                      // icon: Icon(Icons.account_circle),
-                      child: Text("Sign Up",
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16)),
-                    )
-                  ]),
-            ),
-          
+                            fontSize: 16)),
+                  )
+                ]),
+          ),
         ),
         body: TabBarView(
           children: [LoginPage(), SignUp()],
